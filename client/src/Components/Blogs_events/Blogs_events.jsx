dfffdd43a394ddddaca988bf440a2f9b469a6e3f
Fethulmubin <fetihul@gmail.com>
2024-10-16 15:21:20 +0300

    blogs component completed

--- client/src/Components/Blogs_events/Blogs_events.jsx
+++ client/src/Components/Blogs_events/Blogs_events.jsx
@@ -5,16 +5,20 @@ import './Blogs_events.css'
 
 function Blogs_events() {
     return (
-        // <div>
+        <div className='main_wrapper'>
+                <div className='title'>
+                    <h1>አዳዲስ መረጃዎች እና ፈዋኢዶች</h1>
+                </div>
             <div className='wrapper'>
                 <div className='blogsL'>
                     <div className='blog_image'>
                         <img src={madrasa} alt="" />
                     </div>
                     <div className='descriptionL'>
-                        <p>how</p>
-                        <p>yes</p>
-                        <p>beeeee</p>
+                        <h2>የበድር ዘምቻ</h2>
+                        <p style={{textAlign:'justify'}}>በኢስላም ታሪክ ውስጥ ታላቅ ስፍራ የሚሰጠው ታላቁ የበድር ዘመቻ የተደረገበት ለሊት! ...
+                        እውነት በሐሰት ላይ ፣ እምነትም በባዕድ አምልኮ ላይ ድልን መቀዳጀቱ የታወጀባት ታላቅ ለሊት!</p>
+                        <p>Jan 20/2024</p>
                     </div>
                 </div>
                 <div className='blogsR'>
@@ -22,13 +26,13 @@ function Blogs_events() {
                         <img src={student} alt="" />    
                     </div>
                     <div className='descriptionR'>
-                        <p>how</p>
-                        <p>yes</p>
-                        <p>beeeee</p>
+                    <h2>ማዕክላችን</h2>
+                        <p style={{textAlign:'justify'}}>ኣልዒምራን ተቋም የኢስላማዊ ትምህርትን ከዘመን ተሻጋሪ የእስልምና ወግ እና ጥበብ ጋር በማዋሃድ ኢስላማዊ እውቀትን ለማስፋፋት ከተመሠረተበት (1996 አ·ል) ጀምሮ፣</p>
+                        <p>Jan 20/2024</p>
                     </div>
                 </div>
             </div>
-        // </div>
+        </div>
     );
 }
 
