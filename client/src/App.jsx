c1bb29074859d62daaecf94555885811ff189cec
Fethulmubin <fetihul@gmail.com>
2024-10-16 14:14:22 +0300

    edit

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 
 import "./App.css";
+import Blogs_events from "./Components/Blogs_events/Blogs_events";
 import Services from "./Components/Services/Services";
 import Banner from "./Components/Banner/Banner";
 import PrayerTimes from "./Components/Prayer_times/Prayer_time";
@@ -9,9 +10,9 @@ function App() {
   return (
     <>
       <Banner />
-
       <PrayerTimes />
-      <Services />
+      {/* <Services /> */}
+      <Blogs_events/>
     </>
   );
 }
--- client/src/Components/Blogs_events/Blogs_events.jsx
+++ client/src/Components/Blogs_events/Blogs_events.jsx
@@ -1,10 +1,34 @@
 import React from 'react';
+import madrasa from '../../assets/library1.jpg'
+import student from '../../assets/graduate.jpg'
+import './Blogs_events.css'
 
 function Blogs_events() {
     return (
-        <div>
-            <h1>Blogs and Events</h1>
-        </div>
+        // <div>
+            <div className='wrapper'>
+                <div className='blogsL'>
+                    <div className='blog_image'>
+                        <img src={madrasa} alt="" />
+                    </div>
+                    <div className='descriptionL'>
+                        <p>how</p>
+                        <p>yes</p>
+                        <p>beeeee</p>
+                    </div>
+                </div>
+                <div className='blogsR'>
+                    <div className='blog_image'>
+                        <img src={student} alt="" />    
+                    </div>
+                    <div className='descriptionR'>
+                        <p>how</p>
+                        <p>yes</p>
+                        <p>beeeee</p>
+                    </div>
+                </div>
+            </div>
+        // </div>
     );
 }
 
